afa874cbe780b1b1194d40e742d5f8d33da78d35
Sabin Thapa <thapasabin@zohomail.com>
2026-02-14 19:08:42 +0545

    feat: sidekiq wired as ActiveJob adapter

--- api/config/application.rb
+++ api/config/application.rb
@@ -23,5 +23,7 @@ module Api
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # Set Active Job backend
+    config.active_job.queue_adapter = :sidekiq
   end
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  require 'sidekiq/web'
+  Rails.application.routes.draw do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 end
